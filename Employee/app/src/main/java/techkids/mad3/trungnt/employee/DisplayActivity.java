02085ff425a67ad16ea25d8f77d6c99835c0d2e0
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-07 17:17:31 +0700

    UI List Employee (temp)

---
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/DisplayActivity.java
@@ -0,0 +1,16 @@
+package techkids.mad3.trungnt.employee;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrungNT on 4/7/2016.
+ */
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_employee);
+    }
+}
