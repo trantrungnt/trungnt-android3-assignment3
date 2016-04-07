44164f88b7f1113d58b7b32103bc8181020d17fa
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-08 06:15:51 +0700

    define Employee class

---
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/Employee.java
@@ -0,0 +1,34 @@
+package techkids.mad3.trungnt.employee;
+
+/**
+ * Created by TrungNT on 4/8/2016.
+ */
+public class Employee {
+    private String name;
+    private String age;
+    private String birthday;
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+    private Employee()
+    {
+        this.name = "";
+        this.age = "";
+        this.birthday;
+    }
+
+    public Employee(String name, String age, String birthday)
+    {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+}
