a20924464772bc0472ba38504ebb37980d17cec6
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-07 10:25:36 +0700

    UI Add Employee (temp)

---
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/AddEmployeeActivity.java
@@ -0,0 +1,13 @@
+package techkids.mad3.trungnt.employee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee);
+    }
+}
