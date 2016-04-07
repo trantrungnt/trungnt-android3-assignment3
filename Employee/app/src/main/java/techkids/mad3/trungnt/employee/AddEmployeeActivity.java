5d22a7109c5bc630592707f34ee6982655e0bf01
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-08 02:14:03 +0700

    UI Display List Employee (temp)

--- Employee/app/src/main/java/techkids/mad3/trungnt/employee/AddEmployeeActivity.java
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/AddEmployeeActivity.java
@@ -2,12 +2,23 @@ package techkids.mad3.trungnt.employee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddEmployeeActivity extends AppCompatActivity {
+    EditText editTextName, editTextAge, editTextBirthday;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee);
+        initView();
+    }
+
+    private void initView()
+    {
+        btnAdd = (Button) this.findViewById(R.id.btnAdd);
+
     }
 }
