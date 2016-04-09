66b2e599cfd8d1ba7d4b12b91041a80d8ac69ef9
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-10 00:51:00 +0700

    use TextUtils to check isEmpty, define displayAlertDialog function, use Resume method to load data

--- Employee/app/src/main/java/techkids/mad3/trungnt/employee/AddEmployeeActivity.java
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/AddEmployeeActivity.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -25,12 +28,23 @@ public class AddEmployeeActivity extends AppCompatActivity implements View.OnCli
     private Button btnAdd;
     private DatePickerDialog datePickerDialogBirthday;
     private DateFormat dateFormatter;
+    private Context context;
+    private AlertDialog alertDialog;
+    private AlertDialog.Builder alertDialogBuilder;
+    private Employee employee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee);
         initView();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        //load du lieu nang thi nen cho vao ham Resume
         setDataDatePickerDialogBirthday();
     }
 
@@ -53,46 +67,30 @@ public class AddEmployeeActivity extends AppCompatActivity implements View.OnCli
         if (id == R.id.editTextBirthday)
                                 datePickerDialogBirthday.show();
 
+        String name, age, birthday;
+
+        name = editTextName.getText().toString();
+        age = editTextAge.getText().toString();
+        birthday = editTextBirthday.getText().toString();
+
+
         if (id == R.id.btnAdd)
         {
-            if (
-                    editTextName.getText().length() == 0
-                 || editTextName.getText().toString().isEmpty()
-                 || editTextAge.getText().length() == 0
-                 || editTextAge.getText().toString().isEmpty()
-                 || editTextBirthday.getText().length() == 0
-                 || editTextBirthday.getText().toString().isEmpty()
-            )
+            //TextUtils , shared preference, luu vao file, luu vao sqllite, nen dat ten bien chung trong file dimen ///// persistence storage, su khac nhau khi de startACtivity /// flag full screen
+            if ((
+                 //textUntils se kiem tra gia tri null, gia tri empty, gia tri null
+                 TextUtils.isEmpty(name) ||
+                 TextUtils.isEmpty(age) ||
+                 TextUtils.isEmpty(birthday)
+            ) && (displayAlertDialog().isShowing()==false))
 
             {
-                Context context = this;
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-                // set title
-                alertDialogBuilder.setTitle("WARNING ...");
-
-                // set dialog message
-                alertDialogBuilder
-                        .setMessage("You need to type your data!")
-                        .setCancelable(false)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                // show it
-                alertDialog.show();
+                // goi ham alertDialog va show no ra
+                displayAlertDialog().show();
             }else
             {
                 //luu du lieu hoac hien thi du lieu ....
-                String name = editTextName.getText().toString();
-                String age = editTextAge.getText().toString();
-                String birthday = editTextBirthday.getText().toString();
-
-                Employee employee = new Employee(name, age, birthday);
+                employee = new Employee(name, age, birthday);
                 //String emp = employee.getName() + "\n" + employee.getAge() + "\n" + employee.getBirthday();
                 EmployeeManager.getInstance().getArrEmployee().addElement(employee);
 
@@ -117,5 +115,28 @@ public class AddEmployeeActivity extends AppCompatActivity implements View.OnCli
             }
 
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    //dinh nghia phuong thuc Alert Dialog, sau do luc can hien thi Alert Dialog thi show ra
+    private AlertDialog displayAlertDialog()
+    {
+        context = this;
+        alertDialogBuilder = new AlertDialog.Builder(context);
+        // set title
+        alertDialogBuilder.setTitle("WARNING ...");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("You need to type your data!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        alertDialog = alertDialogBuilder.create();
+        return alertDialog;
     }
 }
