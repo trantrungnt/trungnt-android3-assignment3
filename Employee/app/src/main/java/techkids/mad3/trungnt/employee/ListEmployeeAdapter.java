1d4a404106fb6716f7acd03576071f550aee2ee4
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-19 14:11:17 +0700

    Alert Dialog Detail (temp)

--- Employee/app/src/main/java/techkids/mad3/trungnt/employee/ListEmployeeAdapter.java
+++ Employee/app/src/main/java/techkids/mad3/trungnt/employee/ListEmployeeAdapter.java
@@ -1,6 +1,7 @@
 package techkids.mad3.trungnt.employee;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,8 +22,13 @@ public class ListEmployeeAdapter extends BaseAdapter{
     private int layoutItemID;
     private Context mContext;
     private TextView tvName, tvAge, tvJob;
-    private ImageView avatar;
-    private Button btnClose;
+    private ImageView avatar, avatarDetail;
+    private Button btnClose, btnDetail, btnDetailBack;
+    private Dialog dialogDetail;
+    private String titleDialogAlertDetail = "EMPLOYEE INFORMATION";
+    private TextView tvDetailName, tvDetailAge, tvDetailBirthday, tvDetailAddress, tvDetailJob;
+    private Employee employee;
+    private View v;
 
     public ListEmployeeAdapter(Context mContext, int layoutItemID, ArrayList<Employee> arrListEmployee)
     {
@@ -48,7 +54,7 @@ public class ListEmployeeAdapter extends BaseAdapter{
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        Employee employee = getItem(position);
+        employee = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.activity_employee_list_new, parent, false);
@@ -58,6 +64,38 @@ public class ListEmployeeAdapter extends BaseAdapter{
         tvName = (TextView) convertView.findViewById(R.id.txtDisplayNameNew);
         tvAge =(TextView) convertView.findViewById(R.id.txtDisplayAgeNew);
         tvJob = (TextView) convertView.findViewById(R.id.txtDisplayJobNew);
+
+        btnDetail = (Button) convertView.findViewById(R.id.btnDetail);
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogDetail = new Dialog(mContext);
+
+                //su dung template activity_employee_detail cho UI cua Dialog Alert
+                dialogDetail.setContentView(R.layout.activity_employee_detail);
+                //tao title cho AlertDialog
+                dialogDetail.setTitle(titleDialogAlertDetail);
+
+                avatarDetail = (ImageView) dialogDetail.findViewById(R.id.imgEmployeeDetail);
+                tvDetailName = (TextView) dialogDetail.findViewById(R.id.txtDisplayDetailNameNew);
+                tvDetailAge = (TextView) dialogDetail.findViewById(R.id.txtDisplayDetailAge);
+                tvDetailBirthday = (TextView) dialogDetail.findViewById(R.id.txtDisplayDetailBirthday);
+                tvDetailAddress = (TextView) dialogDetail.findViewById(R.id.txtDisplayDetailAddress);
+                tvDetailJob = (TextView) dialogDetail.findViewById(R.id.txtDisplayDetailJob);
+                btnDetailBack = (Button) dialogDetail.findViewById(R.id.btnDetailBack);
+
+                //day du lieu vao AlertDialog
+                avatarDetail.setImageResource(R.drawable.trungnt0);
+                tvDetailName.setText(employee.getName().toString());
+                tvDetailAge.setText(employee.getAge().toString());
+                tvDetailBirthday.setText(employee.getAge().toString());
+                tvDetailAddress.setText(employee.getAddress().toString());
+                tvDetailJob.setText(employee.getJob().toString());
+
+
+                dialogDetail.show();
+            }
+        });
 
         btnClose = (Button) convertView.findViewById(R.id.btnClose);
         btnClose.setOnClickListener(new View.OnClickListener() {
